d975fe61cf2fe6f24ced65efead5d23f793a20bf
Dmitry Andreichyk <Dmitry_Andreichyk@epam.com>
2019-05-16 15:06:46 +0300

    Initial commit

---
+++ src/test/tmp/MainProgram.java
@@ -0,0 +1,11 @@
+package test.tmp;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Main program statred ...");
+		//logic goes here
+		System.out.println("Main program complete");
+	}
+
+}
